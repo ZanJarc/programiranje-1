8e93bb93438ad12846fe7426e762e6b8a5f4d91e
Žan Jarc <zan.jarc@gmail.com>
2019-01-07 23:08:46 +0100

    vaje

--- 11-deli-in-vladaj/vaje/deli_in_vladaj.py
+++ 11-deli-in-vladaj/vaje/deli_in_vladaj.py
@@ -26,7 +26,22 @@
 #     >>> a
 #     [10, 2, 0, 4, 11, 15, 17, 5, 18]
 ##############################################################################
-
+def pivot(a, start, end):
+    element = a[start]
+    for i in range(start, end + 1):
+        if a[i] > element:
+            for j in range(i + 1, end + 1):
+                if a[j] < element:
+                    a[i], a[j] = a[j], a[i]
+    #na tej tocki imamo ze nekako urejen seznam
+    for i in range(start, end + 1):
+        if element < a[i]:
+            #prvic pridemo do elementa, ki je vecji od pivota
+            a[start], a[i- 1] = a[i - 1], a[start]
+            return i - 1
+        
+    a[start], a[end] =  a[end], a[start]            
+    return end
 
 
 ##############################################################################
@@ -42,8 +57,23 @@
 #   >>> quicksort(a)
 #   [2, 3, 4, 5, 10, 11, 15, 17, 18]
 ##############################################################################
+def swamp(a, i, j):
+    a[i], a[j] = a[j], a[i]
 
+def quicksort_part(a, start, end):
+    #a je moja cela tabela, a[start] je pivot, a[end] pa zadnji element dela tabele, ki jo moramo urediti
+    #vsak element je tukaj manjsi od pivota
+    if start >= end:
+        return a
+    else:
+        p = pivot(a, start, end)
+        quicksort_part(a, start, p-1)
+        quicksort_part(a, p+1, end)
+        return a
 
+def quicksort(a):
+    return quicksort_part(a, 0, len(a) -1 )
+     
 
 ##############################################################################
 # V tabeli želimo poiskati vrednost k-tega elementa po velikosti.
